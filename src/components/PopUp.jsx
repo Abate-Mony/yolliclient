1de366681c25ad597197a9c303014db0b6a807a8
mmanuel <bateemma14@gmail.com>
2023-06-05 01:21:12 +0100

    rm err frm code

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -26,9 +26,7 @@ const PopUp = ({ closeModal,setRefresh}) => {
 
         <motion.div
 
-            onClick={() => {
-                setIsScale(true)
-            }}
+
             initial={{ opacity: 0 }}
             animate={{ opacity: 1, transition: { duration: 1 } }}
             exit={{ opacity: 0 }}
